5e77f149c50dd20c203a054b27d51380ca45b487
MUSTAFA ÇAY <mcay51@gmail.com>
2024-09-18 02:23:13 +0300

    servisler config servera bağlanıp ayarlarını config serverdan alacak şekilde ayarlandı.

--- product-service/src/main/java/tr/com/mcay/productservice/service/ProductService.java
+++ product-service/src/main/java/tr/com/mcay/productservice/service/ProductService.java
@@ -2,8 +2,10 @@ package tr.com.mcay.productservice.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 import tr.com.mcay.productservice.model.Product;
 import tr.com.mcay.productservice.repository.ProductRepository;
+import io.github.resilience4j.circuitbreaker.CircuitBreaker;
 
 import java.util.List;
 
@@ -21,18 +23,22 @@ public class ProductService {
     }
 
     public Product getProductById(Long id) {
+
         return productRepository.findById(id).orElse(null);
     }
 
     public Product createProduct(Product product) {
         return productRepository.save(product);
     }
-
+   // @CircuitBreaker(name = "default", fallbackMethod = "fallback")
     public Product updateProduct(Product product) {
         return productRepository.save(product);
     }
 
     public void deleteProduct(Long id) {
         productRepository.deleteById(id);
+    }
+    public String fallback(Long id, Throwable t) {
+        return "Fallback response for product with ID: " + id;
     }
 }
