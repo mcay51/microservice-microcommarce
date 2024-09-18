ad9a095ca5935691e21c82b920d587e3f19826bc
MUSTAFA ÇAY <mcay51@gmail.com>
2024-09-18 22:59:16 +0300

    apigateway üzerinden akış sağlandı. db tablo yapısı güncellendi

--- order-service/src/main/java/tr/com/mcay/orderservice/model/Orders.java
+++ order-service/src/main/java/tr/com/mcay/orderservice/model/Orders.java
@@ -1,15 +1,13 @@
 package tr.com.mcay.orderservice.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
+@Table(name="tbl_orders")
 public class Orders {
 
     @Id
--- product-service/src/main/java/tr/com/mcay/productservice/model/Product.java
+++ product-service/src/main/java/tr/com/mcay/productservice/model/Product.java
@@ -1,15 +1,13 @@
 package tr.com.mcay.productservice.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
+@Table(name="tbl_product")
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
