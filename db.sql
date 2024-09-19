a2ca82dcd26807a65e3e08e01805c7172ae3ea32
MUSTAFA ÇAY <mcay51@gmail.com>
2024-09-19 21:48:17 +0300

    dbsql güncellendi

--- db.sql
+++ db.sql
@@ -1,21 +1,21 @@
-CREATE DATABASE microcommerce;
+CREATE DATABASE orderdb;
 
 \c microcommerce;
 
-CREATE TABLE product (
+CREATE TABLE tbl_product (
                          id SERIAL PRIMARY KEY,
                          name VARCHAR(255) NOT NULL,
-                         stock INTEGER NOT NULL
+                         price INTEGER  NOT NULL
 );
 
-CREATE TABLE "order" (
+CREATE TABLE tbl_orders (
                          id SERIAL PRIMARY KEY,
                          product_id BIGINT NOT NULL,
                          quantity INTEGER NOT NULL,
                          FOREIGN KEY (product_id) REFERENCES product (id)
 );
 
-CREATE TABLE stock (
+CREATE TABLE tbl_stock (
                        id SERIAL PRIMARY KEY,
                        product_id BIGINT NOT NULL,
                        quantity INTEGER NOT NULL,
@@ -24,12 +24,12 @@ CREATE TABLE stock (
 
 
 -- Örnek Veri Eklemek İçin
-INSERT INTO product (name, stock) VALUES ('Product 1', 100);
-INSERT INTO product (name, stock) VALUES ('Product 2', 200);
+INSERT INTO tbl_product (name, price) VALUES ('Product 1', 100);
+INSERT INTO tbl_product (name, price) VALUES ('Product 2', 200);
 
-INSERT INTO stock (product_id, quantity) VALUES (1, 100);
-INSERT INTO stock (product_id, quantity) VALUES (2, 200);
+INSERT INTO tbl_stock (product_id, quantity) VALUES (1, 100);
+INSERT INTO tbl_stock (product_id, quantity) VALUES (2, 200);
 
-INSERT INTO "order" (product_id, quantity) VALUES (1, 5);
-INSERT INTO "order" (product_id, quantity) VALUES (2, 10);
+INSERT INTO tbl_orders (product_id, quantity) VALUES (1, 5);
+INSERT INTO tbl_orders (product_id, quantity) VALUES (2, 10);
 
